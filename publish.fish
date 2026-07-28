5be7d27537d1c82294402933cf3e8c9a20601040
00sapo <00sapo@noreply.codeberg.org>
2026-07-28 16:06:21 +0200

    build: create GitHub releases when publishing

--- publish.fish
+++ publish.fish
@@ -2,6 +2,7 @@ set newversion (sed -n 's/^version = "\(.*\)"$/\1/p' pyproject.toml)
 git commit -am "new v$newversion"
 git tag -a v$newversion -m "v$newversion"
 git push --tags
+rm -rf dist
 uv build
 uv publish --username __token__ --password (rbw get "pypi" "00sapo" -f "token")
 gh release create "v$newversion" --verify-tag --generate-notes --title "v$newversion"
